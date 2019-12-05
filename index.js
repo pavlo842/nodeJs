e1f455c62cba0dd09488e3a7d5ec419e2f3653af
Pavel <pishchalaups@belhard.com>
2019-12-05 16:28:30 +0300

    4-3 create model

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
-        const url = 'mongodb+srv://pavlo:jI6STQNqbc48BjnH@cluster0-mpveb.mongodb.net/test?retryWrites=true&w=majority'
+        const url = 'mongodb+srv://pavlo:jI6STQNqbc48BjnH@cluster0-mpveb.mongodb.net/shop'
         await mongoose.connect(url, {useNewUrlParser: true})
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
--- models/course.js
+++ models/course.js
@@ -1,85 +1,107 @@
-const uuid = require('uuid/v4')
-const fs = require('fs')
-const path = require('path')
+// const uuid = require('uuid/v4')
+// const fs = require('fs')
+// const path = require('path')
 
-class Course {
-    constructor(title, price, img) {
-        this.title = title
-        this.price = price
-        this.img = img
-        this.id = uuid()
-    }
+// class Course {
+//     constructor(title, price, img) {
+//         this.title = title
+//         this.price = price
+//         this.img = img
+//         this.id = uuid()
+//     }
 
-    toJSON() {
-        return {
-            title: this.title,
-            price: this.price,
-            img: this.img,
-            id: this.id
-        }
-    }
+//     toJSON() {
+//         return {
+//             title: this.title,
+//             price: this.price,
+//             img: this.img,
+//             id: this.id
+//         }
+//     }
 
-    static async update(course) {
-        const courses = await Course.getAll()
+//     static async update(course) {
+//         const courses = await Course.getAll()
 
-        const idx = courses.findIndex(c => c.id === course.id)
-        courses[idx] = course
+//         const idx = courses.findIndex(c => c.id === course.id)
+//         courses[idx] = course
 
-        return new Promise((resolve, reject) => {
-            fs.writeFile(
-                path.join(__dirname, '..', 'data', 'courses.json'),
-                JSON.stringify(courses),
-                (err) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve()
-                    }
-                }
-            )
-        })
-    }
+//         return new Promise((resolve, reject) => {
+//             fs.writeFile(
+//                 path.join(__dirname, '..', 'data', 'courses.json'),
+//                 JSON.stringify(courses),
+//                 (err) => {
+//                     if (err) {
+//                         reject(err)
+//                     } else {
+//                         resolve()
+//                     }
+//                 }
+//             )
+//         })
+//     }
 
-    async save() {
-        const courses = await Course.getAll()
-        courses.push(this.toJSON())
+//     async save() {
+//         const courses = await Course.getAll()
+//         courses.push(this.toJSON())
 
-        return new Promise((resolve, reject) => {
-            fs.writeFile(
-                path.join(__dirname, '..', 'data', 'courses.json'),
-                JSON.stringify(courses),
-                (err) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve()
-                    }
-                }
-            )
-        })
-    }
+//         return new Promise((resolve, reject) => {
+//             fs.writeFile(
+//                 path.join(__dirname, '..', 'data', 'courses.json'),
+//                 JSON.stringify(courses),
+//                 (err) => {
+//                     if (err) {
+//                         reject(err)
+//                     } else {
+//                         resolve()
+//                     }
+//                 }
+//             )
+//         })
+//     }
 
-    static getAll() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(
-                path.join(__dirname, '..', 'data', 'courses.json'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve(JSON.parse(content))
-                    }
-                }
-            )
-        })
-    }
+//     static getAll() {
+//         return new Promise((resolve, reject) => {
+//             fs.readFile(
+//                 path.join(__dirname, '..', 'data', 'courses.json'),
+//                 'utf-8',
+//                 (err, content) => {
+//                     if (err) {
+//                         reject(err)
+//                     } else {
+//                         resolve(JSON.parse(content))
+//                     }
+//                 }
+//             )
+//         })
+//     }
 
-    static async getById(id) {
-        const courses = await Course.getAll()
-        return courses.find(c => c.id === id)
-    }
+//     static async getById(id) {
+//         const courses = await Course.getAll()
+//         return courses.find(c => c.id === id)
+//     }
 
-}
+// }
 
-module.exports = Course
+// module.exports = Course
+
+
+
+//                 MONGOOSE
+
+const {Schema, model} = require('mongoose')
+
+const course = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    img: String
+})
+
+
+module.exports = model('Course', course)
+
