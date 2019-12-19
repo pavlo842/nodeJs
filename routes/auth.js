f2a270eb0628ab268c2386894d0775fe0b142458
Pavel <pishchalaups@belhard.com>
2019-12-19 16:25:30 +0300

    5-8 user login

--- routes/auth.js
+++ routes/auth.js
@@ -16,15 +16,32 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    const user = await User.findById('5dea60a35ec7144c1797aca1')
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err => {
-        if (err) {
-            throw err
+    try {
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            const areSame = password === candidate.password
+
+            if (areSame) {
+                req.session.user = candidate
+                req.session.isAuthenticated = true
+                req.session.save(err => {
+                    if (err) {
+                        throw err
+                    }
+                    res.redirect('/')
+                })
+            } else {
+                res.redirect('/auth/login#login')
+            }
+        } else {
+            res.redirect('/auth/login#login')
         }
-        res.redirect('/')
-    })
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 router.post('/register', async (req, res) => {
