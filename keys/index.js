5d106888eefee67d31d8bc56ab4016ed54d090d1
Pavel <pishchalaups@belhard.com>
2019-12-31 11:07:06 +0300

    fix sendgrid error

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     MONGODB_URI: 'mongodb+srv://pavlo842:BJiUVsoVw1XasikA@cluster0-mpveb.mongodb.net/shop',
     SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'nodejs@email.ru',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'pavlo842@email.ru',
     BASE_URL: 'http://localhost:3000'
 }
