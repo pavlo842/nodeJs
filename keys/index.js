9ce43df7519c76fd83e18a19faa1632d3d6e0af5
Pavlo842 <pavlo842@mail.ru>
2020-01-02 17:59:03 +0300

    8-1 key protection

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://pavlo842:BJiUVsoVw1XasikA@cluster0-mpveb.mongodb.net/shop',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'pavlo842@email.ru',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://pavlo842:BJiUVsoVw1XasikA@cluster0-mpveb.mongodb.net/shop',
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'pavlo842@email.ru',
+    BASE_URL: 'http://localhost:3000'
+}
